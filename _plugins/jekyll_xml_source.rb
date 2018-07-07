8cbee1d00c5a393d0d4ce365c60156f6d5756953
Derek Smart <derek@grindaga.com>
2018-07-07 14:30:18 -0400

    save and load xml from cache
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- _plugins/jekyll_xml_source.rb
+++ _plugins/jekyll_xml_source.rb
@@ -7,21 +7,53 @@ module Jekyll_Xml_Source
     safe true
     priority :highest
 
+    def saveToCache(data_source, name, content)
+      path = "#{data_source}/#{name}.json"
+      File.open(path,"w") do |file|
+        file.write(content)
+      end
+    end
+
+    def loadFromCache(data_source, name)
+      path = "#{data_source}/#{name}.json"
+      if not File.exist?(path)
+        return
+      end
+      File.open(path,"r") do |file|
+        return JSON.load(file.read())
+      end
+    end
+
     def generate(site)
       config = site.config['jekyll_xml']
+      data_source = (site.config['data_source'] || '_data')
 
       if !config
         return
       end
 
       config.each do |data|
-        begin
-          result = Net::HTTP.get_response(URI.parse(data['source'])).body
-          site.data[data['data']] = JSON.load(Hash.from_xml(result).to_json)
-        rescue
-          next
+        if data['cache']
+          site.data[data['data']] = loadFromCache(data_source, data['data'])
+          return
         end
+
+        if site.data[data['data']].nil?
+          begin
+            result = Net::HTTP.get_response(URI.parse(data['source'])).body
+            site.data[data['data']] = JSON.load(Hash.from_xml(result).to_json)
+          rescue
+            next
+          end
+
+          if data['cache']
+            saveToCache(data_source, data['data'], Hash.from_xml(result).to_json)
+          end
+        end
+
       end
+
     end
+
   end
 end
